0924e2c7a92015c65f3a81529a95db9abf2789ca
Marius Szczepański <szpanmariusz@gmail.com>
2023-05-31 13:18:10 +0200

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+bool isFemale = true;
+int Age = 29;
+
+if (Age > 30 & name == "Ewa")
+{
+    Console.WriteLine("Jestem Ewa i mam poniżej 30 lat");
+}
+else if (Age < 30 & name == "Marek" & isFemale == false) 
+{
+    Console.WriteLine("Jestem Paulina i mam poniżej 30lat");
+}
+else if (Age == 29 & name == "Ewa" & isFemale == true)
+{
+    Console.WriteLine("Jestem Ewa w wieku 29 lat");
+}
+
