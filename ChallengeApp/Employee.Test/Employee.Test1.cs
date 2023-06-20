2123d1d122ec8ea4bbabe7d2f41df4a4b011b333
Marius Szczepański <szpanmariusz@gmail.com>
2023-06-20 22:10:29 +0200

    Zadanie,dzień 7

---
+++ ChallengeApp/Employee.Test/Employee.Test1.cs
@@ -0,0 +1,27 @@
+
+namespace ChallengeApp.Test
+{
+    public class Tests
+    {
+
+
+        [Test]
+        public void WhenEmployeeCollectTwoScores_ShouldCorrectResult()
+        {
+
+            //arange
+
+            var emp = new Employee("Jaros³aw", "Bary³a", 33);
+            emp.AddScore(5);
+            emp.AddScore(10);
+
+            //act
+            var result = emp.Result;
+
+            //assert
+            Assert.AreEqual(15, result);
+
+         
+        }
+    }
+}
