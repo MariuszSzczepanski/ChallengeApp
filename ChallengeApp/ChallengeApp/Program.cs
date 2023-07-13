2b33ad7de1c79b79d7a9066fee04899fd88fd1f6
Marius Szczepański <szpanmariusz@gmail.com>
2023-07-13 18:56:32 +0200

    Zadanie dzień 9

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,47 +1,12 @@
 ﻿using ChallengeApp;
 
-Employee emp1 = new Employee("Jarosław", "Baryła", 33);
-Employee emp2 = new Employee("Jan", "Kowalski", 35);
-Employee emp3 = new Employee("Marlena", "Hula", 28);
-
-
-
-emp1.AddScore(9);
-emp1.AddScore(4);
-emp1.AddScore(3);
-emp1.AddScore(7);
-emp1.AddScore(5);
-
-emp2.AddScore(5);
-emp2.AddScore(6);
-emp2.AddScore(9);
-emp2.AddScore(8);
-emp2.AddScore(3);
-
-emp3.AddScore(6);
-emp3.AddScore(9);
-emp3.AddScore(6);
-emp3.AddScore(4);
-emp3.AddScore(4);
-
-List<Employee> emps = new List<Employee>()
-{
-    emp1, emp2, emp3
-};
-
-int maxResult = -1;
-Employee empWithMaxResult = null;
-
-foreach (var emp in emps)
-{
-    if(emp.Result > maxResult)
-    {
-        maxResult = emp.Result;
-        empWithMaxResult = emp;
-    }
-}
-
-Console.WriteLine("Pracownik z najwyższą liczbą ocen : " + empWithMaxResult.Name + " " + empWithMaxResult.Surname);
-Console.WriteLine("Liczba punktów : " + empWithMaxResult.Result);
+var employee = new Employee("Mariusz", "Szczepański");
+employee.AddGrade(2);
+employee.AddGrade(4);
+employee.AddGrade(7);
+var statistics = employee.GetStatistics();
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Max : {statistics.Max}");
+Console.WriteLine($"Min : {statistics.Min}");
 
 
---
+++ ChallengeApp/Employee.Test/Employee.Test.cs
@@ -0,0 +1,63 @@
+﻿
+namespace ChallengeApp.Test
+
+{
+    public class EmployeeTests
+    {
+        [Test]
+        public void StatisticsMax()
+        {
+            //arange - przygotowanie
+            var employee = new Employee("Mariusz", "Szczepański");
+            employee.AddGrade(2);
+            employee.AddGrade(4);
+            employee.AddGrade(7);
+
+            //act - uruchamianie
+            var statistics = employee.GetStatistics();
+
+            //assert - sprawdzanie
+            Assert.AreEqual(7, statistics.Max);
+
+
+        }
+
+        [Test]
+        public void StatisticsMin()
+        {
+            //arange - przygotowanie
+            var employee = new Employee("Mariusz", "Szczepański");
+            employee.AddGrade(2);
+            employee.AddGrade(4);
+            employee.AddGrade(7);
+
+            //act - uruchamianie
+            var statistics = employee.GetStatistics();
+
+            //assert - sprawdzanie
+            Assert.AreEqual(2, statistics.Min);
+
+
+        }
+
+        [Test]
+        public void StatisticsAverage()
+        {
+            //arange - przygotowanie
+            var employee = new Employee("Mariusz", "Szczepański");
+            employee.AddGrade(2);
+            employee.AddGrade(4);
+            employee.AddGrade(7);
+
+            //act - uruchamianie
+            var statistics = employee.GetStatistics();
+
+            //assert - sprawdzanie
+            Assert.AreEqual(Math.Round(4.33, 2), Math.Round(statistics.Average, 2));
+
+
+        }
+
+
+    }
+}
