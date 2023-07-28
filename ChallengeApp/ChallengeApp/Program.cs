4edd626c7ff5ee6270a76c1b6f6a72806eb6dd5c
Marius Szczepański <szpanmariusz@gmail.com>
2023-07-28 11:51:36 +0200

    Zadanie dzień 11

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -2,10 +2,32 @@
 
 var employee = new Employee("Mariusz", "Szczepański");
 employee.AddGrade("Mariusz");
-employee.AddGrade(4000);
+employee.AddGrade("9");
 employee.AddGrade(7);
 employee.AddGrade(5);
+
 var statistics = employee.GetStatistics();
+var statistics2 = employee.GetStatisticsWithFor();
+var statistics3 = employee.GetStatisticsWithDoWhile();
+var statistics4 = employee.GetStatisticsWithWhile();
+
+
+Console.WriteLine("Metoda Foreach");
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Max : {statistics.Max}");
+Console.WriteLine($"Min : {statistics.Min}");
+
+Console.WriteLine("Metoda For");
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Max : {statistics.Max}");
+Console.WriteLine($"Min : {statistics.Min}");
+
+Console.WriteLine("Metoda Do While");
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Max : {statistics.Max}");
+Console.WriteLine($"Min : {statistics.Min}");
+
+Console.WriteLine("Metoda While");
 Console.WriteLine($"Average: {statistics.Average:N2}");
 Console.WriteLine($"Max : {statistics.Max}");
 Console.WriteLine($"Min : {statistics.Min}");
