90c506baf449d898aac4768a8752b3065c6026ca
Marius Szczepański <szpanmariusz@gmail.com>
2023-07-31 17:03:00 +0200

    Uporządkowany kod po dniu 11

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -7,27 +7,8 @@ employee.AddGrade(7);
 employee.AddGrade(5);
 
 var statistics = employee.GetStatistics();
-var statistics2 = employee.GetStatisticsWithFor();
-var statistics3 = employee.GetStatisticsWithDoWhile();
-var statistics4 = employee.GetStatisticsWithWhile();
-
 
 Console.WriteLine("Metoda Foreach");
-Console.WriteLine($"Average: {statistics.Average:N2}");
-Console.WriteLine($"Max : {statistics.Max}");
-Console.WriteLine($"Min : {statistics.Min}");
-
-Console.WriteLine("Metoda For");
-Console.WriteLine($"Average: {statistics.Average:N2}");
-Console.WriteLine($"Max : {statistics.Max}");
-Console.WriteLine($"Min : {statistics.Min}");
-
-Console.WriteLine("Metoda Do While");
-Console.WriteLine($"Average: {statistics.Average:N2}");
-Console.WriteLine($"Max : {statistics.Max}");
-Console.WriteLine($"Min : {statistics.Min}");
-
-Console.WriteLine("Metoda While");
 Console.WriteLine($"Average: {statistics.Average:N2}");
 Console.WriteLine($"Max : {statistics.Max}");
 Console.WriteLine($"Min : {statistics.Min}");
